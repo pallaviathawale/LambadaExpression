f3b3d23b325fe7d3d2027a7a713da953b808b1e0
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-30 20:06:52 +0530

    [PALLAVI]Add: RetrieveRecords

--- LambadaPracticeProblem/Program.cs
+++ LambadaPracticeProblem/Program.cs
@@ -4,24 +4,13 @@
     {
         static void Main(string[] args)
         {
-            List<Student> studentList = new List<Student>
-        {
-            new Student { Id = 1, Name = "Pallavi", PhoneNumber = "7218533707", Address = "akola", Age = 25 },
-            new Student { Id = 2, Name = "Rashmi", PhoneNumber = "97522358", Address = "amaravati", Age = 26 },
-            new Student { Id = 3, Name = "Nisha", PhoneNumber = "98635174", Address = "mumbai", Age = 25 }
-        };
+            Console.WriteLine("Welcome To Lambda Expression!!!\n");
 
-            Console.WriteLine("Student Details:");
-            studentList.ForEach(student =>
-            {
-                Console.WriteLine($"ID: {student.Id}");
-                Console.WriteLine($"Name: {student.Name}");
-                Console.WriteLine($"Phone Number: {student.PhoneNumber}");
-                Console.WriteLine($"Address: {student.Address}");
-                Console.WriteLine($"Age: {student.Age}");
-                Console.WriteLine();
-            });
+            List<Student> students = new List<Student>();
+            StudentDataManegement.AddStudentData(students);
+            StudentDataManegement.RetrieveBetweenRange(students);
         }
     }
+    
     
 }
--- LambadaPracticeProblem/Student.cs
+++ LambadaPracticeProblem/Student.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace LambadaPracticeProblem
 {
-   public class Student
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public Student(int Id, string Name, string PhoneNumber, string Address, int Age)
         {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string PhoneNumber { get; set; }
-            public string Address { get; set; }
-            public int Age { get; set; }
+            this.Id = Id;
+            this.Name = Name;
+            this.PhoneNumber = PhoneNumber;
+            this.Address = Address;
+            this.Age = Age;
+        }
     }
+
 }
