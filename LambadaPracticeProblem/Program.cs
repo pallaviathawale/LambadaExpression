302b7c24ae01c4596dac3314378ca90daf2c663e
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-30 20:19:17 +0530

    [PALLAVI] Add: RetrieveAllRecords

--- LambadaPracticeProblem/Program.cs
+++ LambadaPracticeProblem/Program.cs
@@ -4,13 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Lambda Expression!!!\n");
-
             List<Student> students = new List<Student>();
-            StudentDataManegement.AddStudentData(students);
-            StudentDataManegement.RetrieveBetweenRange(students);
+
+            // Add default values to the list
+            students.Add(new Student { Id = 1, Name = "Pallavi", PhoneNumber = "7218533707", Address = "Akola", Age = 25 });
+            students.Add(new Student { Id = 2, Name = "Pooja", PhoneNumber = "922429072", Address = "Amaravati", Age = 23 });
+            students.Add(new Student { Id = 3, Name = "Rashmi", PhoneNumber = "9856734567", Address = "Anjangaon", Age = 18 });
+            students.Add(new Student { Id = 4, Name = "Rutuja", PhoneNumber = "789654231", Address = "mumbai", Age = 14 });
+            students.Add(new Student { Id = 5, Name = "Nikhil", PhoneNumber = "9527616356", Address = "nagpur", Age = 16 });
+
+            // Display all student details
+            Console.WriteLine("All Student Details:");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Phone Number: {student.PhoneNumber}, Address: {student.Address}, Age: {student.Age}");
+            }
+
+            //Retrieve all records from the list for age between 12 to 18 using LINQ
+            var ageFilteredStudents = students.Where(student => student.Age >= 12 && student.Age <= 18);
+            Console.WriteLine("\nStudents between the age of 12 and 18:");
+            foreach (var student in ageFilteredStudents)
+            {
+                Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Phone Number: {student.PhoneNumber}, Address: {student.Address}, Age: {student.Age}");
+            }
         }
     }
-    
+
     
 }
--- LambadaPracticeProblem/Student.cs
+++ LambadaPracticeProblem/Student.cs
@@ -7,21 +7,12 @@ using System.Threading.Tasks;
 namespace LambadaPracticeProblem
 {
     public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public int Age { get; set; }
-
-        public Student(int Id, string Name, string PhoneNumber, string Address, int Age)
         {
-            this.Id = Id;
-            this.Name = Name;
-            this.PhoneNumber = PhoneNumber;
-            this.Address = Address;
-            this.Age = Age;
-        }
-    }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Address { get; set; }
+            public int Age { get; set; }
 
+    }
 }
